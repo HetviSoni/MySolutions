469dbd80f911ffd718afcd207f3a61517edd5a9d
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2022-05-27 23:54:39 +0530

    Time: 5 ms (54.94%), Space: 41.4 MB (91.13%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,37 @@
+class Solution {
+    static int f(int i,int buy, int t, int[] arr, int[][][] dp)
+    {
+        if(t==0)
+            return 0;
+        if(i==arr.length)
+            return 0;
+        if(dp[i][buy][t]!=-1)
+            return dp[i][buy][t];
+        int profit=0;
+        if(buy==1)
+        {
+            int b= -arr[i]+f(i+1,0,t,arr,dp);
+            int nb=f(i+1,1,t,arr,dp);
+            profit=Math.max(b,nb);
+        }
+        else
+        {
+            int s=arr[i]+f(i+1,1,t-1,arr,dp);
+            int ns=f(i+1,0,t,arr,dp);
+            profit=Math.max(s,ns);
+        }
+        return dp[i][buy][t]=profit;
+    }
+    public int maxProfit(int k, int[] prices) {
+        int dp[][][]= new int[prices.length][2][k+1];
+        for(int i=0;i<prices.length;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                for(int l=0;l<=k;l++)
+                    dp[i][j][l]=-1;
+            }
+        }
+        return f(0,1, k,prices, dp);
+    }
+}
