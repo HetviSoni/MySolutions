e2c3653ee3b1ccf7774b94b0e4283bf363fc9298
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2022-06-16 15:13:47 +0530

    Time: 941 ms (7.71%), Space: 43.3 MB (60.74%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,37 @@
+class Solution {
+    static boolean isPali(String a, int s, int e)
+    {
+        while(s<=e)
+        {
+            if(a.charAt(s)!=a.charAt(e))
+                return false;
+            s++;
+            e--;
+        }
+        return true;
+    }
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int max=0;
+        int start=0;
+        int end=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n-1;j>i;j--)
+            {
+                {
+                    if(j-i+1>max)
+                    {
+                        if(isPali(s,i,j))
+                        {
+                            max=j-i+1;
+                            start=i;
+                            end=j;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substring(start,end+1);
+    }
+}
