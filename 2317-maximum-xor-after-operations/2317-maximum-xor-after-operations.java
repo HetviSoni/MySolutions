b6c5f5df70379ff55210bee5fafee111d6951e42
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2022-06-25 22:56:37 +0530

    Time: 2 ms (66.67%), Space: 73.3 MB (66.67%) - LeetHub

---
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maximumXOR(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++)
+        {   xor|=nums[i];
+            // System.out.println(xor);
+        }
+        return xor;
+    }
+}
