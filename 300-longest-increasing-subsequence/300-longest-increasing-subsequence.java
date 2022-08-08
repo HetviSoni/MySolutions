a9d179be7246ee6b2e330ad89271dc9e2f0fb48b
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2022-08-08 14:03:31 +0530

    Time: 716 ms (7.01%), Space: 242.1 MB (6.15%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,26 +1,29 @@
 class Solution {
-    static int f(int prev, int i, int[] nums,int [][] dp)
+    static int f(int prev,int i, int arr[], int dp[][])
     {
-        if(i==nums.length)
-            return 0;
-        if(prev==-1)
-            return Math.max(1+f(i,i+1,nums,dp),f(-1,i+1,nums,dp));
-        else
+        if(i==arr.length)
         {
-            if(dp[prev][i]!=-1)
-                return dp[prev][i];
-            int take=0,nt=0;
-            if(nums[i]>nums[prev])
-                take=1+f(i,i+1,nums,dp);
-            return dp[prev][i]=Math.max(f(prev,i+1,nums,dp),take);
+            return 0;
         }
-        
+        if(prev==-1)
+            return Math.max(1+f(i,i+1,arr,dp),f(-1,i+1,arr,dp));
+        if(dp[prev][i]!=-1)
+            return dp[prev][i];
+        int take=0;
+        int nt= f(prev,i+1,arr,dp);
+        if(arr[i] >arr[prev])
+            take = 1+f(i,i+1,arr,dp);
+        return dp[prev][i]=Math.max(take,nt);
     }
     public int lengthOfLIS(int[] nums) {
         int dp[][]= new int[nums.length][nums.length];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        
+        for(int i=0;i<dp.length;i++)
+        {
+            for(int j=0;j<dp.length;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
         return f(-1,0,nums,dp);
     }
 }
