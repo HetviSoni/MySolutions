3a3666ccdd450acdfb895c5a90d2f5a3488d6a4d
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2023-01-05 18:58:01 +0530

    Update 2244-MinimumRoundstoCompleteAllTasks.java
    Added solution

--- 2244-Minim-Rounds-to-Complete-All-Tasks/2244-MinimumRoundstoCompleteAllTasks.java
+++ 2244-Minim-Rounds-to-Complete-All-Tasks/2244-MinimumRoundstoCompleteAllTasks.java
@@ -1,1 +1,35 @@
-
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        Arrays.sort(tasks);
+        int ans=0;
+        for(int i=0;i<tasks.length;i++)
+        {
+            int count=0;
+            int cur=tasks[i];
+            int p=i;
+            while(p<tasks.length && tasks[p]==cur)
+            {
+                count++;
+                p++;
+            }
+            i=p-1;
+            if(count==1)
+                return -1;
+            else 
+            { 
+                // System.out.println(count);
+                ans+=(int)Math.ceil((double)count/(double)3);
+                // System.out.println("adding "+Math.ceil((double)count/(double)3)+" at"+i);
+            }
+        }
+        return ans
+        ;
+    }
+}
+// 2 2 2 3 3 4 4 4 4 4
+// 2--1
+// 3--1
+// 4--2
+// 5--2
+// 6--2
+// 7--3
