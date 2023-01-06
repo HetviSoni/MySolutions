193dba32125472722d93d0cd1a20065e121712d9
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2023-01-06 11:09:19 +0530

    Update 1833MaximumIceCreamBars.java
    added solution

--- 1833-Maximum-Ice-Cream-Bars/1833MaximumIceCreamBars.java
+++ 1833-Maximum-Ice-Cream-Bars/1833MaximumIceCreamBars.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int bill=0;
+        int count=0;
+        while(count<costs.length && bill<coins)
+        {
+            if(costs[count]+bill<=coins)
+            {
+                bill+=costs[count];
+                count++;
+            }
+            else break;
+        }
+        return count;
+    }
+}
