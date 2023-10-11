9bc238b790a70c22f1f7ae357c6e9c0d6088ae5d
HetviSoni <76479104+HetviSoni@users.noreply.github.com>
2023-10-11 17:48:23 +0530

    Added solution - LeetHub

---
+++ Sum of nodes on the longest path from root to leaf node - GFG/sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.java
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+		    Solution obj = new Solution();
+		    int res = obj.sumOfLongRootToLeafPath(root);
+		    System.out.println(res);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+node class of binary tree
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+    // sum length
+    
+    // path =0
+    // sum =0
+    
+    // path=2
+    // sum=13
+    
+    // path=3
+    
+    
+    static int sum;
+    static int path;
+    
+    public static void traverse(Node root,int current_path_length,int current_sum)
+    {
+        if(root.left==null && root.right==null)
+        {
+            if(current_path_length>path)
+            {
+                path=current_path_length;
+                sum=current_sum+root.data;
+                
+            }
+            if(current_path_length==path)
+            {
+                sum=Math.max(sum,current_sum+root.data);
+            }
+        }
+        
+        current_path_length++;
+        current_sum+=root.data;
+        if(root.left!=null)
+        traverse(root.left,current_path_length,current_sum);
+        if(root.right!=null)
+        traverse(root.right,current_path_length,current_sum);
+        current_path_length--;
+        current_sum-=root.data;
+    }
+    
+    
+    public int sumOfLongRootToLeafPath(Node root)
+    {
+        //code here
+        sum=0;
+        path=0;
+        traverse(root,0,0);
+        return sum;
+        
+    }
+}
